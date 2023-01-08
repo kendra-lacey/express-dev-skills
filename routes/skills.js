ef7ed090be80d8baa0e547debe6900e6997a9cc0
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-01-08 18:46:54 -0500

    implement controller

--- routes/skills.js
+++ routes/skills.js
@@ -1,14 +1,11 @@
 import { Router} from 'express'
-import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
 /* GET SKILLS INDEX. */
-router.get('/', function (req, res) {
-  res.render('skills/index', {
-    skills: skills
-  })
-})
+
+router.get('/', skillsCtrl.index)
 
 export {
   router
