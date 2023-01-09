4ceea36e138a86139bc4d57671c110cf85e0d9ea
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-01-08 21:00:27 -0500

    add new controller create new ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,12 +1,22 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from '../models/skill.js'
 
-
-function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+function index (req, res) {
+  // use the model to find all skills
+  Skill.find({}) // returns a promise -> use .then
+  .then(skills => {
+    // respond by rending a view with skills
+    res.rendeer('skills/index', {
+      skills: skills
+    })
+  })
+  // handle errors
+  .catch (error => {
+    console.log(error)
+    res.redirect('/')
   })
 }
 
+
 export {
-  index
+  index,
 }
