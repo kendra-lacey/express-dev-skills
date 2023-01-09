98ef8325a1902537317b2220b91910e6cc449e59
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-01-08 19:21:05 -0500

    add skill model and export

--- models/skill.js
+++ models/skill.js
@@ -5,4 +5,12 @@ const Schema = mongoose.Schema
 
 const skillSchema = new Schema({
   text: String,
+  knowledge: Boolean
 })
+
+// COMPILE SCHEMA INTO A MODEL AND EXPORT IT
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+  Skill
+}
